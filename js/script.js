e343792f4796c83156e7a963a36a6708c0d3fe44
RickCraco <riccardocracolici43@gmail.com>
2023-11-07 17:20:01 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ createApp({
     mounted(){
         for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
-                this.emails.push(response.data.response);
+                this.emails.push(response.data);
             })
         }
     }
