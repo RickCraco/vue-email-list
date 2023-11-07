0d49a4b64445691e848d3a5e0b9672edd1dd6e62
RickCraco <riccardocracolici43@gmail.com>
2023-11-07 17:15:03 +0100

    aggiunta funzione al mounted per raccolta dati tramite API

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            emails:[]
+        }
+    },
+    methods:{
+
+    },
+    mounted(){
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(response => {
+                this.emails.push(response.data.response);
+            })
+        }
+    }
+}).mount('#app');
